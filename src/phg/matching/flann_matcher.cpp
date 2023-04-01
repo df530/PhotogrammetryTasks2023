b4ffb4bd9420e163df8b025969c4d51388524759
df530 <fild10@yandex.ru>
2023-04-01 22:13:58 +0300

    implement task02

--- src/phg/matching/flann_matcher.cpp
+++ src/phg/matching/flann_matcher.cpp
@@ -6,8 +6,8 @@
 phg::FlannMatcher::FlannMatcher()
 {
     // параметры для приближенного поиска
-//    index_params = flannKdTreeIndexParams(TODO);
-//    search_params = flannKsTreeSearchParams(TODO);
+    index_params = flannKdTreeIndexParams(4);
+    search_params = flannKsTreeSearchParams(32);
 }
 
 void phg::FlannMatcher::train(const cv::Mat &train_desc)
@@ -17,5 +17,15 @@ void phg::FlannMatcher::train(const cv::Mat &train_desc)
 
 void phg::FlannMatcher::knnMatch(const cv::Mat &query_desc, std::vector<std::vector<cv::DMatch>> &matches, int k) const
 {
-    throw std::runtime_error("not implemented yet");
+    int n = query_desc.rows;
+    cv::Mat indices(n, k, CV_32SC1);
+    cv::Mat distances(n, k, CV_32FC1);
+    flann_index->knnSearch(query_desc, indices, distances, k, *search_params);
+
+    matches.resize(n);
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < k; ++j) {
+            matches[i].emplace_back(i, indices.at<int>(i, j), distances.at<float>(i, j));
+        }
+    }
 }
--- src/phg/sfm/panorama_stitcher.cpp
+++ src/phg/sfm/panorama_stitcher.cpp
@@ -23,7 +23,31 @@ cv::Mat phg::stitchPanorama(const std::vector<cv::Mat> &imgs,
     {
         // здесь надо посчитать вектор Hs
         // при этом можно обойтись n_images - 1 вызовами функтора homography_builder
-        throw std::runtime_error("not implemented yet");
+        std::vector<int> stack(n_images);
+        for (int i = 0; i < n_images; ++i) stack[i] = i;
+
+        std::vector<bool> found(n_images, false);
+        while (!stack.empty()) {
+            int cur = stack.back();
+
+            if (found[cur]) {
+                stack.pop_back();
+                continue;
+            }
+
+            int curParent = parent[cur];
+            if (curParent == -1) {
+                Hs[cur] = cv::Mat::eye(3, 3, CV_64F);
+                found[cur] = true;
+                stack.pop_back();
+            } else if (found[curParent]) {
+                Hs[cur] = Hs[curParent] * homography_builder(imgs[cur], imgs[curParent]);
+                found[cur] = true;
+                stack.pop_back();
+            } else {
+                stack.push_back(curParent);
+            }
+        }
     }
 
     bbox2<double, cv::Point2d> bbox;
@@ -46,19 +70,19 @@ cv::Mat phg::stitchPanorama(const std::vector<cv::Mat> &imgs,
     // из-за растяжения пикселей при использовании прямой матрицы гомографии после отображения между пикселями остается пустое пространство
     // лучше использовать обратную и для каждого пикселя на итоговвой картинке проверять, с какой картинки он может получить цвет
     // тогда в некоторых пикселях цвет будет дублироваться, но изображение будет непрерывным
-//        for (int i = 0; i < n_images; ++i) {
-//            for (int y = 0; y < imgs[i].rows; ++y) {
-//                for (int x = 0; x < imgs[i].cols; ++x) {
-//                    cv::Vec3b color = imgs[i].at<cv::Vec3b>(y, x);
-//
-//                    cv::Point2d pt_dst = applyH(cv::Point2d(x, y), Hs[i]) - bbox.min();
-//                    int y_dst = std::max(0, std::min((int) std::round(pt_dst.y), result_height - 1));
-//                    int x_dst = std::max(0, std::min((int) std::round(pt_dst.x), result_width - 1));
-//
-//                    result.at<cv::Vec3b>(y_dst, x_dst) = color;
-//                }
-//            }
-//        }
+    for (int i = 0; i < n_images; ++i) {
+        for (int y = 0; y < imgs[i].rows; ++y) {
+            for (int x = 0; x < imgs[i].cols; ++x) {
+                cv::Vec3b color = imgs[i].at<cv::Vec3b>(y, x);
+
+                cv::Point2d pt_dst = transformPoint(cv::Point2d(x, y), Hs[i]) - bbox.min();
+                int y_dst = std::max(0, std::min((int) std::round(pt_dst.y), result_height - 1));
+                int x_dst = std::max(0, std::min((int) std::round(pt_dst.x), result_width - 1));
+
+                result.at<cv::Vec3b>(y_dst, x_dst) = color;
+            }
+        }
+    }
 
     std::vector<cv::Mat> Hs_inv;
     std::transform(Hs.begin(), Hs.end(), std::back_inserter(Hs_inv), [&](const cv::Mat &H){ return H.inv(); });
